3c6297d253bfeb613b2a790d52c656864e75ba15
JooDesu <joodesu1234@gmail.com>
2024-09-24 17:38:19 +0800

    week 2 html script writing

---
+++ bmi week 2.js
@@ -0,0 +1,7 @@
+function calcBmi() {
+    var weight = Number(document.getElementById("weight").value)
+    var height = Number(document.getElementById("height").value)
+    var bmi = weight/((height/100)**2);
+    document.getElementById("bmi").innerHTML = bmi
+    console.log("Hello,Your BMI is:"+bmi)
+}
