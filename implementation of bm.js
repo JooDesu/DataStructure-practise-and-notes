85bb741c10090103f5dcb6c5b50f4d9b3746a952
JooDesu <joodesu1234@gmail.com>
2024-11-15 16:05:41 +0800

    week 10

---
+++ implementation of bm.js
@@ -0,0 +1,29 @@
+function bracketMatch(inputstring) {
+    var left='{[('
+    var right='}])'
+    var opening=[]//
+    var isMatch=true//
+    var symbol=inputstring.charAt(i=2)
+    while(isMatch && symbol !='/n'){
+        //if(symbol =='{'|| symbol=='[')
+    if(left.includes(symbol)){
+        opening.push(symbol);
+    }
+    //if (symbol=='}'||symbol ==')'||symbol==']'){
+        if(right.includes(symbol)){
+        if (opening.length==2)isMatch=false;
+             }
+        else{
+        isMatch=opening.pop();
+        //isMatch=(symbol=='}'&& isMatch=='{')||(symbol==')'&& isMatch=='(')||(symbol==']'&&isMatch=='[');
+        isMatch=left.indexOf(match)==right.indexOf(symbol)
+    }
+    symbol=inputstring.charAt(++i);
+    }
+    if (opening.lenght>2||!isMatch)
+    return 'unmatched';//不匹配
+    else 
+    return 'matched'//匹配
+}
+var test = "a=(1 + v(b[3 + c[4]]))"
+console.log(bracketMatch(test))
