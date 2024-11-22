ef93ce89d08c618753b838b64f3e673c00245479
JooDesu <joodesu1234@gmail.com>
2024-11-22 13:37:42 +0800

    2dArrayhtml.js

--- 2dArrayhtml.js
+++ 2dArrayhtml.js
@@ -11,19 +11,19 @@ var ary2d=[
 // [1][1],[_row][_col],[1][3]=>[_row][_col-1],[_row][_col],[_row][_col+1]
 // [2][1],[2][2],[2][3]      =>[_row+1][_col-1],[_row+1][_col],[_row+1][_col+1]
 
-var row=ary2d.length;
-var col=ary2d[0].length;
+var row=ary2d.length;//宣告row的值
+var col=ary2d[0].length;//宣告columm的值
 
 //draw map
-var canvas = document.getElementById("map").getContext("2d");
-var size=Math.min(canvas.canvas.height/row, canvas.canvas.width/col);
+var canvas = document.getElementById("map").getContext("2d");//從2dArrayhtml所設定的格子和數量放入該程式裏
+var size=Math.min(canvas.canvas.height/row, canvas.canvas.width/col);//該圖面積的大小和格子數量
 for(var _row=0;_row<row;_row++){
     for(var _col=0;_col<col;_col++){
         //ar2d[_row][_col]=>0,1
         if(ary2d[_row][_col]==1){
-            canvas.fillStyle="#0000ff"
+            canvas.fillStyle="#0000ff"//上顔色
         }else{
-            canvas.fillStyle="#000000"
+            canvas.fillStyle="#000000"//上色
         }
         //600/5=>120  coordinate x,y , width, heigth
         canvas.fillRect(_col*size,_row*size,size,size);
@@ -31,7 +31,7 @@ for(var _row=0;_row<row;_row++){
     }
 }
 
-function neighbor(_row,_col){
+function neighbor(_row,_col){//格子周圍的鄰居
    var count=0;
    //visit 8 neighbor
 
