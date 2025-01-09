84ba7ed92baeb9b59c73c6d984c1711d2c45a904
JooDesu <joodesu1234@gmail.com>
2025-01-09 23:26:34 +0800

    bracket matching

--- bracketmatching .js
+++ bracketmatching .js
@@ -6,17 +6,17 @@ function bracketMatch(inputstring) {
     var symbol=inputstring.charAt(i=2)
     while(isMatch && symbol !='/n'){//判斷是否相同
         //if(symbol =='{'|| symbol=='[')
-    if(left.includes(symbol)){
-        opening.push(symbol);
+    if(left.includes(symbol)){//包括相同
+        opening.push(symbol);//放入
     }
     //if (symbol=='}'||symbol ==')'||symbol==']'){
         if(right.includes(symbol)){
         if (opening.length==2)isMatch=false;
              }
         else{
-        isMatch=opening.pop();
+        isMatch=opening.pop();//移除
         //isMatch=(symbol=='}'&& isMatch=='{')||(symbol==')'&& isMatch=='(')||(symbol==']'&&isMatch=='[');
-        isMatch=left.indexOf(isMatch)==right.indexOf(symbol)
+        isMatch=left.indexOf(isMatch)==right.indexOf(symbol)//左邊和右邊的值或符號相同
     }
     symbol=inputstring.charAt(++i);
     }
